6f3b452360042d44188ff32c9f742e2bef0ab34e
zenna <97446606+zenna9@users.noreply.github.com>
2025-02-05 20:54:12 +0900

    ✅ easy-useSearch 수정

--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -98,7 +98,9 @@ it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다',
 });
 
 it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {
-  expect(result.current.filteredEvents).toEqual(events);
+  act(() => result.current.setSearchTerm('회의'));
+  expect(result.current.filteredEvents).toEqual([events[0]]);
+
   act(() => result.current.setSearchTerm('점심'));
   expect(result.current.filteredEvents).toEqual([events[1]]);
 });
