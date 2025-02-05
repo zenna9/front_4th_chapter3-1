a19204841b7141a1d9f2327bc7bc61b0ae5b31e5
zenna <97446606+zenna9@users.noreply.github.com>
2025-02-05 19:28:27 +0900

    medium시작

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react';
+import { act, screen, renderHook, waitFor } from '@testing-library/react';
 import { http, HttpResponse } from 'msw';
 
 import {
@@ -8,11 +8,44 @@ import {
 } from '../../__mocks__/handlersUtils.ts';
 import { useEventOperations } from '../../hooks/useEventOperations.ts';
 import { server } from '../../setupTests.ts';
-import { Event } from '../../types.ts';
+import { Event, EventForm } from '../../types.ts';
+import { setupServer } from 'msw/node';
+import { events } from '../../__mocks__/response/events.json';
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {});
+beforeAll(() => server.listen());
+afterAll(() => server.close());
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다(GET)', async () => {
+  const { result } = renderHook(() => useEventOperations(false)); //useEventOperations를 실행하여 훅을 테스트할 수 있도록 함.
+  await act(async () => {
+    await result.current.fetchEvents(); // get요청 실행
+  });
+  expect(result.current.events).toEqual(events);
+});
+
+it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다(POST)', async () => {
+  let expectedMessage = '';
+  const { result } = renderHook(() => useEventOperations(false));
+  const eventData: Event = {
+    id: '',
+    title: '이 거지같은 회사에 두번씩이나 버려지다니',
+    date: '2025-02-19',
+    startTime: '08:30',
+    endTime: '18:00',
+    description: '월급주잖아..',
+    location: '강남역',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 1, // 분 단위로 저장
+  };
+  await act(async () => {
+    await result.current.saveEvent(eventData);
+  });
+  console.log(result);
+  await waitFor(() => {
+    expect(screen.getByText('일정이 추가되었습니다.')).toBeInTheDocument();
+  });
+});
 
 it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
 
